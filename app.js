ce5f59ead936eefbf72275f502ff3559045e0835
Ivan Duka <ivan.duka.pm@gmail.com>
2020-01-10 21:10:38 -0700

    2

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-var exec = require("child_process").exec;
+// var exec = require("child_process").exec;
 
-const file = "./file.pdf";
+// const file = "./file.pdf";
 
-var cmd = `java -jar ./buildvu-html-trial.jar ${file} ./`;
+// var cmd = `java -jar ./buildvu-html-trial.jar ${file} ./`;
 
-exec(cmd, function(error, stdout, stderr) {
-  console.log(stdout);
-  console.log(stderr);
+// exec(cmd, function(error, stdout, stderr) {
+//   console.log(stdout);
+//   console.log(stderr);
 
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Done!");
-  }
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Done!");
+//   }
+// });
+
+const glob = require("glob");
+const { exec } = require("child_process");
+
+const pdfFolder = "./pdf/";
+const outFolder = "./out/"
+
+const files = glob.sync(pdfFolder + "**/*.pdf");
+
+const cmd = file => `java -jar ./buildvu-html-trial.jar ${file} ${outFolder}`;
+
+files.forEach(file => {
+  exec(cmd(file), (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Done!");
+    }
+  });
 });
