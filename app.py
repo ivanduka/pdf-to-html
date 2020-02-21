7907a4709fe004ec7b472c2065a3b2a57c09daeb
Ivan Duka <ivan.duka.pm@gmail.com>
2020-02-20 20:46:25 -0700

    process

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from PyPDF2 import PdfFileReader
 
 if __name__ == "__main__":
-    input1 = PdfFileReader(open("./pdf/A78970-2_Supplemental_ESA_-_Part_1_of_3_-_A5E4Z2.pdf", 'rb'))
+    input1 = PdfFileReader(
+        open("./pdf/A78970-2_Supplemental_ESA_-_Part_1_of_3_-_A5E4Z2.pdf", 'rb'))
     size = input1.getPage(0).mediaBox
-    print(size)
+    print(size.getWidth(), size.getHeight())
+
+# table_areas accepts strings of the form x1,y1,x2,y2
+# where (x1, y1) -> top-left and (x2, y2) -> bottom-right in PDF coordinate space.
+# In PDF coordinate space, the bottom-left corner of the page is the origin, with coordinates (0, 0).
--- out/A78970-2_Supplemental_ESA_-_Part_1_of_3_-_A5E4Z2/process.js
+++ out/A78970-2_Supplemental_ESA_-_Part_1_of_3_-_A5E4Z2/process.js
@@ -1,13 +1,18 @@
 const obj = {};
 
-const mouseup = e => {
+const mouseUp = e => {
   const rect = e.target.getBoundingClientRect();
   const x = e.clientX - rect.left; //x position within the element.
   const y = e.clientY - rect.top; //y position within the element.
-  console.log(`${obj.x}-${obj.y} => ${x}-${y}`);
+  console.log(
+    `${Number.parseInt(obj.x, 10)}-${Number.parseInt(
+      obj.y,
+      10
+    )} => ${Number.parseInt(x, 10)}-${Number.parseInt(y, 10)}`
+  );
 };
 
-const mousedown = e => {
+const mouseDown = e => {
   const rect = e.target.getBoundingClientRect();
   const x = e.clientX - rect.left; //x position within the element.
   const y = e.clientY - rect.top; //y position within the element.
@@ -15,7 +20,75 @@ const mousedown = e => {
   obj.y = y;
 };
 
-const a = document.querySelector("#page1");
-console.log(a.offsetWidth, a.offsetHeight);
-a.addEventListener("mousedown", mousedown);
-a.addEventListener("mouseup", mouseup);
+// setTimeout(() => console.clear(), 5000);
+
+const page1 = document.querySelector("#page1");
+
+page1.addEventListener("mousedown", mouseDown);
+page1.addEventListener("mouseup", mouseUp);
+
+const canvasElement = document.createElement("canvas");
+
+setTimeout(() => {
+  console.clear();
+  new ResizeObserver(() => {
+    console.log("SIZE:", page1.offsetWidth, page1.offsetHeight);
+    canvasElement.style.width = page1.style.width;
+    canvasElement.style.height = page1.style.height;
+  }).observe(page1);
+}, 5000);
+
+page1.appendChild(canvasElement);
+
+const ctx = canvasElement.getContext("2d");
+
+const rect = canvasElement.getBoundingClientRect();
+const canvasx = document.body.scrollLeft + rect.left;
+const canvasy = document.body.scrollTop + rect.top;
+
+let last_mousex = 0;
+let last_mousey = 0;
+let mousex = 0;
+let mousey = 0;
+let mousedown = false;
+
+//Mousedown
+page1.addEventListener("mousedown", e => {
+  last_mousex = e.clientX - canvasx;
+  last_mousey = e.clientY - canvasy;
+  mousedown = true;
+});
+
+//Mouseup
+page1.addEventListener("mouseup", e => {
+  mousedown = false;
+});
+
+//Mousemove
+page1.addEventListener("mousemove", e => {
+  mousex = parseInt(e.clientX - canvasx);
+  mousey = parseInt(e.clientY - canvasy);
+  if (mousedown) {
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height); //clear canvas
+    ctx.beginPath();
+    var width = mousex - last_mousex;
+    var height = mousey - last_mousey;
+    ctx.rect(last_mousex, last_mousey, width, height);
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+  //Output
+  // console.log(
+  //   "current: " +
+  //     mousex +
+  //     ", " +
+  //     mousey +
+  //     "\t\t\tlast: " +
+  //     last_mousex +
+  //     ", " +
+  //     last_mousey +
+  //     "\t\t\tmousedown: " +
+  //     mousedown
+  // );
+});
